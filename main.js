faf724b6dc4d908d54e56be390e58e912052bc86
abduvokhidoff <abcdefgmananam@gmail.com>
2024-10-10 17:45:14 +0500

    done

---
+++ main.js
@@ -0,0 +1,65 @@
+
+let buttons = document.querySelectorAll('button')
+let ps = document.querySelectorAll('p')
+let spans = document.querySelectorAll('span')
+
+buttons[0].addEventListener('click', () => {
+    let result = prompt(`Qiyriq Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Qiyriq Set jami narxi ${45000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[1].addEventListener('click', () => {
+    let result = prompt(`Snek Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Snek Set jami narxi ${79000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[2].addEventListener('click', () => {
+    let result = prompt(`Klassik Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Klassik Set jami narxi ${34000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[3].addEventListener('click', () => {
+    let result = prompt(`Longer Rings Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Longer Rings Set jami narxi ${45000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[4].addEventListener('click', () => {
+    let result = prompt(`Big Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Big Set jami narxi ${62000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[5].addEventListener('click', () => {
+    let result = prompt(`Lester Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Lester Set jami narxi ${53000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[6].addEventListener('click', () => {
+    let result = prompt(`Skul Set nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Skul Set jami narxi ${36000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[7].addEventListener('click', () => {
+    let result = prompt(`Kombo Set Big nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Kombo Set Big jami narxi ${20000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[8].addEventListener('click', () => {
+    let result = prompt(`Dostlar 1x nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Dostlar 1x jami narxi ${43000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[9].addEventListener('click', () => {
+    let result = prompt(`Dostlar 2x nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Dostlar 2x jami narxi ${79000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[10].addEventListener('click', () => {
+    let result = prompt(`Dostlar 4x nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Dostlar 4x jami narxi ${149000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
+buttons[11].addEventListener('click', () => {
+    let result = prompt(`Dostlar 1x nechta zakaz qilmoqchisiz`)
+    alert(`${result}ta Dostlar 1x jami narxi ${43000*result} som\n Mahsulotni Zakaz Qilmoqchisiz`)
+    alert('Zakazingiz qabul qilindi')
+})
